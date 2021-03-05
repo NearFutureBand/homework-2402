1eac927a2fbbd84c3a21a323f2055b8de82abfe7
Pavel Belyakov <theflashofsonic@gmail.com>
2021-03-05 21:08:51 +0300

    manual router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
 import Ex1 from './components/Ex1';
 import Ex2 from './components/Ex2';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-function App() {
-  return (
-    <>
-      <Ex1 />
-      <Ex2 />
-      <LoginForm />
-    </>
-  );
+// home, about, contacs
+
+class App extends Component {
+
+  state = {
+    currentPage: 'home'
+  }
+
+  renderPage = () => {
+    switch (this.state.currentPage) {
+      case 'home': {
+        return <PageHome />
+      }
+      case 'about': {
+        return <PageAbout />
+      }
+      case 'contacts': {
+        return <PageContacts />
+      }
+    }
+  }
+
+  goToRoute = (nextRoute) => {
+    this.setState({ currentPage: nextRoute });
+  }
+
+  render () {
+    return (
+      <div>
+        <button onClick={() => this.goToRoute('home') }>Home</button>
+        <button onClick={() => this.goToRoute('about')}>About</button>
+        <button onClick={() => this.goToRoute('contacts')}>Contacts</button>
+        {this.renderPage()}
+      </div>
+    )
+  }
 }
+
+const PageAbout = () => {
+  return <div className="page">About</div>
+}
+
+const PageHome = () => (
+  <div className="page">
+    <div>Home</div>
+  </div>
+)
+
+const PageContacts = () => <div className="page">Contacts</div>;
 
 export default App;
